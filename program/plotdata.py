d4729195fb6fe7138622188232d25e19e6d5ef2c
Jordan Maulana <jordanmaulana25@gmail.com>
2020-04-03 06:01:08 +0700

    Update plotdata.py

--- program/plotdata.py
+++ program/plotdata.py
@@ -26,50 +26,21 @@ kNN = neighbors.KNeighborsClassifier(n_neighbors = k, weights='distance')
 kNN.fit(X_train, Y_train)
 prediksi_kNN = kNN.predict(X_test)
 acc_kNN = accuracy_score(Y_test, prediksi_kNN)
-print('Akurasi k-NN:', acc_kNN)
+print('Accuracy k-NN:', acc_kNN)
 
 gnb = GaussianNB()
 prediksi_NB = gnb.fit(X_train, Y_train).predict(X_test)
 acc_NB = accuracy_score(Y_test, prediksi_NB)
-print('Akurasi Naive Bayes:', acc_NB)
+print('Accuracy Naive Bayes:', acc_NB)
 
 DT = tree.DecisionTreeClassifier()
 DT = DT.fit(X_train, Y_train)
 prediksi_DT = DT.predict(X_test)
 acc_DT = accuracy_score(Y_test, prediksi_DT)
-print('Akurasi DT:', acc_DT)
+print('Accuracy DT:', acc_DT)
 
 dSVM = svm.SVC(gamma='auto') # one versus one SVM
 dSVM.fit(X_train, Y_train)
 prediksi_SVM = dSVM.predict(X_test)
 acc_SVM = accuracy_score(Y_test, prediksi_SVM)
-print('Akurasi SVM:', acc_SVM)
-
-ser = serial.Serial('COM4',9600)
-
-counter = 0
-start = False
-now = 200
-
-while True:
-    line = ser.readline().decode().strip()
-    c = int(line)
-    if start == True:
-            counter += 1
-    if c>200 :
-        if c > now:
-            now = c
-        else:
-            if start == True:
-                print(counter)
-                print('k-NN:', kNN.predict([[counter]]))
-                print('Naive Bayes:', gnb.predict([[counter]]))
-                print('DT:', DT.predict([[counter]]))
-                print('SVM:', dSVM.predict([[counter]]))
-                print()
-                counter = 0
-                now = 200
-
-            start = True
-    else:
-        now = 200
+print('Accuracy SVM:', acc_SVM)
